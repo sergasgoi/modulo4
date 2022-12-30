2d23c9701a2fc46d4d144e6a24b1e4da2a080266
sergasgoi <sergasgoi@hotmail.com>
2022-12-30 00:23:16 -0300

    tpn1

---
+++ tpn1/frontend/src/App.js
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Nav from "./components/layout/Nav";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+// import './App.css';
+
+import HomePage from './pages/HomePage';
+import NosotrosPage from './pages/NosotrosPage';
+import ProductosPage from './pages/ProductosPage';
+import ProductosPage2 from './pages/ProductosPage2';
+import ProductosPage3 from './pages/ProductosPage3';
+import ContactoPage from './pages/ContactoPage';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header/>
+
+      <BrowserRouter>      
+      <Nav/> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="nosotros" element={<NosotrosPage />} />
+        <Route path="productos" element={<ProductosPage />} />
+        <Route path="gorras" element={<ProductosPage2 />} />
+        <Route path="abrigos" element={<ProductosPage3 />} />
+        <Route path="contacto" element={<ContactoPage />} />
+      </Routes>      
+      </BrowserRouter>
+
+
+      <Footer/>
+      
+      <br/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tpn1/frontend/src/pages/HomePage.js
@@ -0,0 +1,51 @@
+import React from 'react';
+// import '../styles/home.css'
+
+const HomePage = (props) => {
+    return (
+        <main className="home">
+
+            <div className="contenido">
+                <div className="imagen">
+                    Imagen
+                    <p>Lorem ipsum dolor, sit amet consectetur adiet.</p>
+
+                </div>
+                <div className="tityley">
+                    <div className="titulo">
+                        Titulo
+                    </div>
+                    <div className="leyenda">
+                        <p>Lorem ipsum doimos dolor accusamus neque laborum atque accusantium perspiciatis, in illo itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique facere unde quos necessitatibus quia culpa
+                            eligendi consesam?  laboriosam magni voluptatem enim eveniet.
+                            Lorem ipsum doimos dolor osam magni voluptatem enim eveniet</p>
+
+                    </div>
+                </div>
+            </div>
+            <div className="contenido">
+                <div className="imagen">
+                    Imagen
+                    <p>Lorem ipsum dolor, sit amet consectetur adiet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adiet.</p>
+
+                </div>
+                <div className="tityley">
+                    <div className="titulo">
+                        Titulo
+                    </div>
+                    <div className="leyenda">
+                        <p>Lorem ipsum doimos dolor accusamus neque laborum atque accusantium perspiciatis, in illo itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique facere unde quos necessitatibus quia culpa
+                            eligendi consesam?  laboriosam magni voluptatem enim eveniet.
+                            Lorem ipsum doimos dolor accusamagni voluptatem enim eveniet</p>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </main>
+    )
+}
+
+export default HomePage;
